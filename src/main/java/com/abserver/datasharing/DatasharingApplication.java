f217998ac6a740957d1b1ccb457a67ac25f48c99
thaisBertoldi <thais_brt@hotmail.com>
2023-05-14 19:14:34 -0300

    init: Project started

---
+++ src/main/java/com/abserver/datasharing/DatasharingApplication.java
@@ -0,0 +1,13 @@
+package com.abserver.datasharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abserver/datasharing/DatasharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abserver.datasharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
