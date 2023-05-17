e9e943c2ae309591cebcc93c060c10f26aba46af
thaisBertoldi <thais_brt@hotmail.com>
2023-05-17 18:32:28 -0300

    feat: created POST method

--- src/main/java/com/abserver/datasharing/controller/CustomerController.java
+++ src/main/java/com/abserver/datasharing/controller/CustomerController.java
@@ -5,10 +5,7 @@ import com.abserver.datasharing.dto.CustomerDTO;
 import com.abserver.datasharing.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,10 @@ public class CustomerController {
     @GetMapping
     public ResponseEntity<List<CustomerDTO>> findAll(){
         return ResponseEntity.ok(service.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        return ResponseEntity.ok(new CustomerDTO(service.createCustomer(customerDTO)));
     }
 }
