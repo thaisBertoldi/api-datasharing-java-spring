51b86507c677f15c586da7b6a79062e5722ca9bb
thaisBertoldi <thais_brt@hotmail.com>
2023-05-28 20:29:27 -0300

    feat: created method POST and FindById for Address

---
+++ src/main/java/com/abserver/datasharing/service/AddressService.java
@@ -0,0 +1,37 @@
+package com.abserver.datasharing.service;
+
+import com.abserver.datasharing.domain.Address;
+import com.abserver.datasharing.repository.AddressRepository;
+import com.abserver.datasharing.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    public Address create(Address address) {
+        Address newAddress = new Address(
+            null,
+            address.getStreet(),
+            address.getDistrict(),
+            address.getNumber(),
+            address.getZipCode(),
+            address.getCity(),
+            address.getState(),
+            address.getCountry()
+        );
+        return addressRepository.save(newAddress);
+    }
+
+    public Address findById(Integer id) {
+        Optional<Address> obj = addressRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Object not found! ID: " + id + ", Type: " + Address.class.getName()));
+    }
+
+}
