a233703f8c5aad6249a8019e6641389d05feeb05
thaisBertoldi <thais_brt@hotmail.com>
2023-05-31 19:40:14 -0300

    feat: created entity Appointment and your repository

---
+++ src/main/java/com/abserver/datasharing/domain/Appointment.java
@@ -0,0 +1,12 @@
+package com.abserver.datasharing.domain;
+
+import java.util.Date;
+
+public class Appointment {
+
+    private Integer id;
+    private Customer customer;
+    private Date initialSchedule;
+    private Date finalSchedule;
+
+}
