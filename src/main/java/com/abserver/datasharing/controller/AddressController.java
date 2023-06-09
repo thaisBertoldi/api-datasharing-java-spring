4140fe2137b9b8781998f21427da6ff790b50d65
thaisBertoldi <thais_brt@hotmail.com>
2023-06-09 20:23:26 -0300

    feat: configuration ApiInfo and security for swagger

--- src/main/java/com/abserver/datasharing/controller/AddressController.java
+++ src/main/java/com/abserver/datasharing/controller/AddressController.java
@@ -2,6 +2,10 @@ package com.abserver.datasharing.controller;
 
 import com.abserver.datasharing.domain.Address;
 import com.abserver.datasharing.service.AddressService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
