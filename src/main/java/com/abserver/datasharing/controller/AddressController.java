84f8bed33ac76d3c0cde07cbf657209654763e53
Tiago Almeida <42592613+TiagoAlmeidaPoa@users.noreply.github.com>
2023-06-09 20:26:56 -0300

    deleted imports unused

--- src/main/java/com/abserver/datasharing/controller/AddressController.java
+++ src/main/java/com/abserver/datasharing/controller/AddressController.java
@@ -2,10 +2,6 @@ package com.abserver.datasharing.controller;
 
 import com.abserver.datasharing.domain.Address;
 import com.abserver.datasharing.service.AddressService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
