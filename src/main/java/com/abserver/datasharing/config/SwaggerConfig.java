ea34b422223b12122b4c0363a462a8f0e0227c67
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-06-23 19:57:32 -0300

    feat: creating test profile

--- src/main/java/com/abserver/datasharing/config/SwaggerConfig.java
+++ src/main/java/com/abserver/datasharing/config/SwaggerConfig.java
@@ -1,9 +1,9 @@
 package com.abserver.datasharing.config;
 
-import com.google.common.net.HttpHeaders;
 import io.swagger.models.auth.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,14 +14,11 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
